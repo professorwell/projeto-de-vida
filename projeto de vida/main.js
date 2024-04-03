1d871cbf5fc3d7d6ed7c0d6ac4eb425b929fa864
professorwell <141196100+professorwell@users.noreply.github.com>
2024-04-03 16:38:19 -0300

    aula 6

--- projeto de vida/main.js
+++ projeto de vida/main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo"); 
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
@@ -14,5 +14,18 @@ for(let i=0;i <botoes.length;i++){
 // document.querySelector("#minhaDiv p").textContent = "chega de calor";
 const contadores = document.querySelectorAll(".contador");
 let tempoAtual = new Date();
-const tempoObjetivo1 = new Date ("2024-11-23T00:00:00");
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2024-05-29T00:00:00");
+
+function calculaTempo(tempoObjetivo) {
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
